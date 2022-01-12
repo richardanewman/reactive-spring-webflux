454ac454b8376a3f7f502302bdaaf6cb1751bb6c
richardanewman <rick@richardnewman.dev>
2022-01-11 17:19:30 -0700

    adds log for reactive stream events and creates flux test.

--- reactive-programming-using-reactor/src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
+++ reactive-programming-using-reactor/src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
@@ -1,7 +1,32 @@
 package com.learnreactiveprogramming.service;
 
+import org.junit.jupiter.api.Test;
+import reactor.test.StepVerifier;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class FluxAndMonoGeneratorServiceTest {
 
+    FluxAndMonoGeneratorService fluxAndMonoGeneratorService = new FluxAndMonoGeneratorService();
+
+    @Test
+    void namesFlux() {
+
+        var namesFlux = fluxAndMonoGeneratorService.namesFlux();
+        //step verifier expectNext only. create automatically invokes subscribe
+        StepVerifier.create(namesFlux)
+                .expectNext("alex", "ben", "cloe")
+                .verifyComplete();
+
+        //step verifier expectNextCount only
+        StepVerifier.create(namesFlux)
+                .expectNextCount(3)
+                .verifyComplete();
+
+        //mixing expectNext and expectNextCount: equal to all expected
+        StepVerifier.create(namesFlux)
+                .expectNext("alex")
+                .expectNextCount(2)
+                .verifyComplete();
+    }
 }
